82177b73987defe3b8c42d4e4b9b913b1db73ada
Chaurasiyabhi <101700862+Chaurasiyabhi@users.noreply.github.com>
2022-05-17 19:15:59 +0530

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,24 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int front=0,Rear=0;
+    int isQueueEmpty();
+
+    if(front==Rear){
+        return 1;
+        return -1;
+    }
+    void dequeue(){
+        if(isQueueEmpty()==1)
+        cout<<"Queue is Empty:"<<endl;
+    }
+    else {
+        cout<<"Dequeue elements";
+        fornt++;
+    }
+}
+    return 0;
+}
+
